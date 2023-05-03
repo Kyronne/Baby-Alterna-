2d5b22b089bf7f55cb43aa71aef3f3d7a4e8938b
Kyronne <kyronne.parkes@gmail.com>
2023-05-03 13:05:27 +0100

    Added more links

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,13 @@
     export const navLinks = [
     {
+        id: "https://bscscan.com/token/0x01d03ac619875e85ae1069187d2d766229e83603",
+        title: "Contract",
+    },
+    {
+      id: "https://pancakeswap.finance/swap?inputCurrency=0x01D03ac619875E85AE1069187d2d766229E83603&outputCurrency=BNB",
+      title: "Buy",
+    },
+    {
       id: "https://twitter.com/BabyAlternaBSC",
       title: "Twitter",
     },
@@ -15,5 +23,6 @@
       id: "mailto:contact@babyalterna.com",
       title: "Email",
     },
+    
   ];
   
